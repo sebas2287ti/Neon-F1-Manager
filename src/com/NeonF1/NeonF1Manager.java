475bfadc8b94792bf111f8f0c8f58096d55ec4a6
sebas2287ti <Sebas2287ti@gmail.com>
2026-05-15 20:31:10 -0500

    Estructura Basica

---
+++ src/com/NeonF1/NeonF1Manager.java
@@ -0,0 +1,21 @@
+package com.NeonF1;
+
+import javax.swing.*;
+import com.NeonF1.infrastructure.ui.window.UiNeonF1Manager;
+import java.awt.*;
+
+public class NeonF1Manager {
+    public static void main(String[] args) {
+        UiNeonF1Manager ui = new UiNeonF1Manager();
+        JFrame window = new JFrame("Formula 1 Datos Generales");
+
+
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setContentPane(ui.getMainPanel());
+        window.setResizable(false);
+        window.pack();
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+    }
+}
---
+++ src/com/NeonF1/infrastructure/ui/window/UiNeonF1Manager.java
@@ -0,0 +1,12 @@
+package com.NeonF1.infrastructure.ui.window;
+
+import javax.swing.*;
+
+public class UiNeonF1Manager {
+
+
+
+    public JPanel getMainPanel() {
+        return new JPanel();
+    }
+}
