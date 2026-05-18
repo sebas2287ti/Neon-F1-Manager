519e039c63b45c22f0fc4ef25ce23725f3d7fcc2
sebas2287ti <Sebas2287ti@gmail.com>
2026-05-18 18:56:10 -0500

    Metodo Buscador totalmente terminado

--- src/com/NeonF1/domain/DAO/PilotF1DAO.java
+++ src/com/NeonF1/domain/DAO/PilotF1DAO.java
@@ -6,7 +6,10 @@ import com.NeonF1.infrastructure.persistence.DataConnection;
 import java.awt.*;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class PilotF1DAO {
@@ -39,9 +42,10 @@ public class PilotF1DAO {
         return true;
     }
 
-    public static void BuscarPilotF1 (PilotF1 FiltrosPilotF1) {
+    public static List BuscarPilotF1(PilotF1 FiltrosPilotF1) {
         StringBuilder StringBuscarPilotF1 = new StringBuilder("SELECT * FROM \"DatosF1\" WHERE 1 = 1");
         List<Object> Filtros = new ArrayList<>();
+        List<PilotF1> DBData = new ArrayList<>();
 
         if (FiltrosPilotF1.getAño() != null) {
             StringBuscarPilotF1.append(" AND \"Año\" = ?");
@@ -103,9 +107,36 @@ public class PilotF1DAO {
             Filtros.add(FiltrosPilotF1.getTotal_Carreras());
         }
 
+        try (Connection connection = DataConnection.getConnection(); PreparedStatement Filtrado = connection.prepareStatement(StringBuscarPilotF1.toString())) {
+            for (int i = 0; i < Filtros.size(); i++) {
+                Filtrado.setObject(i + 1, Filtros.get(i));
+            }
 
+            try (ResultSet resultSet = Filtrado.executeQuery()) {
+                while (resultSet.next()) {
+                    PilotF1 DBDatos = new PilotF1();
 
+                    DBDatos.setId(resultSet.getInt("id")); // O el tipo de dato que uses para tu ID
+                    DBDatos.setAño(resultSet.getInt("Año"));
+                    DBDatos.setEquipo(resultSet.getString("Equipo"));
+                    DBDatos.setNombre(resultSet.getString("Nombre"));
+                    DBDatos.setN_Piloto(resultSet.getInt("N_Piloto"));
+                    DBDatos.setPilotoPrincipal(resultSet.getBoolean("PilotoPrincipal"));
+                    DBDatos.setGanadorMundial(resultSet.getBoolean("GanarMundialPilotos"));
+                    DBDatos.setPosicionCampeonato(resultSet.getInt("PosicionCampeonato"));
+                    DBDatos.setPuntosCampeonato(resultSet.getInt("PuntosCampeonato"));
+                    DBDatos.setVictorias(resultSet.getInt("Victorias"));
+                    DBDatos.setPoles(resultSet.getInt("Poles"));
+                    DBDatos.setN_campeonatos(resultSet.getInt("N_Campeonato"));
+                    DBDatos.setTotal_Carreras(resultSet.getInt("TotalCarreras"));
 
-
+                    DBData.add(DBDatos);
+                }
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        System.out.println(DBData.toString());
+        return DBData;
     }
 }
