0447db5296de085d5c9338f007332bfec50786ec
sebas2287ti <Sebas2287ti@gmail.com>
2026-05-18 17:26:54 -0500

    PilotDao INSERT

--- src/com/NeonF1/domain/DAO/PilotF1DAO.java
+++ src/com/NeonF1/domain/DAO/PilotF1DAO.java
@@ -1,12 +1,42 @@
 package com.NeonF1.domain.DAO;
 
+import com.NeonF1.domain.entities.PilotF1;
+import com.NeonF1.infrastructure.persistence.DataConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 
 public class PilotF1DAO {
-    public static boolean InsertPilot () {
-        return false;
+    public static boolean InsertPilot (PilotF1 NewPilotF1) {
+        String StringInsert = "INSERT INTO \"DatosF1\" (\"Año\", \"Equipo\", \"Nombre\", \"N_Piloto\", \"PilotoPrincipal\", \"GanarMundialPilotos\", \"PosicionCampeonato\", \"PuntosCampeonato\", \"Victorias\", \"Poles\", \"N_Campeonato\", \"TotalCarreras\") VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+
+
+        try (Connection connection = DataConnection.getConnection(); PreparedStatement Insert = connection.prepareStatement(StringInsert)) {
+
+            Insert.setInt(1, NewPilotF1.getAño());
+            Insert.setString(2, NewPilotF1.getEquipo());
+            Insert.setString(3, NewPilotF1.getNombre());
+            Insert.setInt(4, NewPilotF1.getN_Piloto());
+            Insert.setBoolean(5, NewPilotF1.getPilotoPrincipal());
+            Insert.setBoolean(6, NewPilotF1.getGanadorMundial());
+            Insert.setInt(7, NewPilotF1.getPosicionCampeonato());
+            Insert.setInt(8, NewPilotF1.getPuntosCampeonato());
+            Insert.setInt(9, NewPilotF1.getVictorias());
+            Insert.setInt(10, NewPilotF1.getPoles());
+            Insert.setInt(11, NewPilotF1.getN_campeonatos());
+            Insert.setInt(12, NewPilotF1.getTotal_Carreras());
+
+            Insert.executeUpdate();
+
+        }
+        catch (Exception e) {
+            return false;
+        }
+        return true;
     }
-    public static ArrayList BuscarF1() {
-        return new ArrayList();
+    public static ArrayList BuscarPilotF1 () {
+        return new ArrayList(null);
     }
 }
