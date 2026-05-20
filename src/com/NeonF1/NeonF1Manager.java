08c075d4f9c42b71e9241e9ee88fccd5e8a417a5
sebas2287ti <Sebas2287ti@gmail.com>
2026-05-19 23:33:19 -0500

    Correcion del main para inciar el contenedor

--- src/com/NeonF1/NeonF1Manager.java
+++ src/com/NeonF1/NeonF1Manager.java
@@ -1,25 +1,28 @@
 package com.NeonF1;
 
 import javax.swing.*;
+
+import com.NeonF1.infrastructure.ui.components.ControlerInterface;
 import com.NeonF1.infrastructure.ui.window.UiNeonF1Manager;
 import com.NeonF1.infrastructure.persistence.DataConnection;
 import com.NeonF1.domain.DAO.PilotF1DAO;
+import com.sun.jdi.InterfaceType;
+
 import java.awt.*;
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class NeonF1Manager {
     public static void main(String[] args) {
-        UiNeonF1Manager ui = new UiNeonF1Manager();
-        JFrame window = new JFrame("Formula 1 Datos Generales");
+        SwingUtilities.invokeLater(() -> {
+            JFrame window = new JFrame("F1 Manager");
+            window.setSize(1280, 720);
+            window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            window.setLocationRelativeTo(null);
 
+            ControlerInterface controlador = new ControlerInterface(window);
 
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setContentPane(ui.getMainPanel());
-        window.setResizable(false);
-        window.pack();
-        window.setLocationRelativeTo(null);
-        window.setVisible(true);
-
+            window.setVisible(true);
+        });
     }
 }
