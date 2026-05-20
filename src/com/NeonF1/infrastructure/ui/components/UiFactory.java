a87106c3be3cdca2677a26b1ac44a18c4202dbfb
sebas2287ti <Sebas2287ti@gmail.com>
2026-05-20 00:05:39 -0500

    Fixed Class UiFactory and Method CrearBoton

--- src/com/NeonF1/infrastructure/ui/components/UiFactory.java
+++ src/com/NeonF1/infrastructure/ui/components/UiFactory.java
@@ -7,14 +7,14 @@ import java.util.List;
 public class UiFactory {
 
     public static JButton CrearBotom (String simbolo, int x, int y, int w, int h, Color Background, Color Text, Color Lateral) {
-        JButton Boton = new JButton();
+        JButton Boton = new JButton(simbolo);
 
         Boton.setBounds(x, y, w, h);
         Boton.setBackground(Background);
         Boton.setForeground(Text);
         Boton.setFont(new Font("SansSerif", Font.BOLD, 24));
         Boton.setFocusPainted(false);
-        Boton.setBorder(BorderFactory.createLineBorder(Lateral));
+        Boton.setBorder(BorderFactory.createLineBorder(Lateral, 1));
 
         Boton.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseEntered(java.awt.event.MouseEvent e) { Boton.setBackground(Lateral); }
