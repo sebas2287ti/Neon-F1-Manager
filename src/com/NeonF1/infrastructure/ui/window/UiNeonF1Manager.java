1d2efc27a8f8bcb50150a72ccd5129a982604cde
sebas2287ti <Sebas2287ti@gmail.com>
2026-05-19 23:33:01 -0500

    Contenedor

--- src/com/NeonF1/infrastructure/ui/window/UiNeonF1Manager.java
+++ src/com/NeonF1/infrastructure/ui/window/UiNeonF1Manager.java
@@ -1,9 +1,15 @@
 package com.NeonF1.infrastructure.ui.window;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class UiNeonF1Manager {
+    private CardLayout cardLayout;
+    private JPanel mainContainer;
 
+    public UiNeonF1Manager() {
+
+    }
 
 
     public JPanel getMainPanel() {
