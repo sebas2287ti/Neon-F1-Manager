cf7c595d3ac60c352446b494884ba8f53332fa1b
sebas2287ti <Sebas2287ti@gmail.com>
2026-05-20 02:59:48 -0500

    Fix in swing Jframe

--- src/com/NeonF1/NeonF1Manager.java
+++ src/com/NeonF1/NeonF1Manager.java
@@ -16,9 +16,10 @@ public class NeonF1Manager {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             JFrame window = new JFrame("F1 Manager");
-            window.setSize(1280, 720);
+                window.setSize(1280, 720);
             window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             window.setLocationRelativeTo(null);
+            window.setResizable(false);
 
             ControlerInterface controlador = new ControlerInterface(window);
 
