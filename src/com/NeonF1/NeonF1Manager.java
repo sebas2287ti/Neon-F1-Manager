3777cf29a501e57e4ad55b65beb93f3565d2e451
sebas2287ti <Sebas2287ti@gmail.com>
2026-05-21 22:15:23 -0500

    Se eliminaron imports que no se usaban

--- src/com/NeonF1/NeonF1Manager.java
+++ src/com/NeonF1/NeonF1Manager.java
@@ -3,14 +3,6 @@ package com.NeonF1;
 import javax.swing.*;
 
 import com.NeonF1.infrastructure.ui.components.ControlerInterface;
-import com.NeonF1.infrastructure.ui.window.UiNeonF1Manager;
-import com.NeonF1.infrastructure.persistence.DataConnection;
-import com.NeonF1.domain.DAO.PilotF1DAO;
-import com.sun.jdi.InterfaceType;
-
-import java.awt.*;
-import java.sql.Connection;
-import java.sql.SQLException;
 
 public class NeonF1Manager {
     public static void main(String[] args) {
