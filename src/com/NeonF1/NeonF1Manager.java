e650af21999801cdbf0b256f14a679eb1f2be42e
sebas2287ti <Sebas2287ti@gmail.com>
2026-05-22 08:06:33 -0500

    Comentarios y arreglo de la estructura y migracion de variables a estandar java e ingles

--- src/com/NeonF1/NeonF1Manager.java
+++ src/com/NeonF1/NeonF1Manager.java
@@ -1,21 +1,24 @@
 package com.NeonF1;
 
-import javax.swing.*;
-
 import com.NeonF1.infrastructure.ui.components.ControlerInterface;
+
+import javax.swing.*;
 
 public class NeonF1Manager {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            JFrame window = new JFrame("F1 Manager");
-                window.setSize(1280, 720);
-            window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            window.setLocationRelativeTo(null);
-            window.setResizable(false);
 
-            ControlerInterface controlador = new ControlerInterface(window);
+            //Creacion de la ventana JFrame
+            JFrame windowMain = new JFrame("F1 Manager");
+            windowMain.setSize(1280, 720);
+            windowMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            windowMain.setLocationRelativeTo(null);
+            windowMain.setResizable(false);
 
-            window.setVisible(true);
+            //Iniciar el controlador de todos los paneles dentro de la ventana principal
+            ControlerInterface controler = new ControlerInterface(windowMain);
+
+            windowMain.setVisible(true);
         });
     }
 }
