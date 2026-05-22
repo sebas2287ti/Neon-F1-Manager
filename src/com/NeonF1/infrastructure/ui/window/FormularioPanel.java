c759919e48cf66a6d1d7c7bba4f785ab752177a2
sebas2287ti <Sebas2287ti@gmail.com>
2026-05-21 22:19:00 -0500

    Se realiza la comunicacion con la BD

--- src/com/NeonF1/infrastructure/ui/window/FormularioPanel.java
+++ src/com/NeonF1/infrastructure/ui/window/FormularioPanel.java
@@ -1,36 +1,64 @@
-package com.NeonF1.infrastructure.ui.window;
+    package com.NeonF1.infrastructure.ui.window;
 
-import com.NeonF1.infrastructure.ui.components.Background;
-import com.NeonF1.infrastructure.ui.components.ControlerInterface;
-import com.NeonF1.infrastructure.ui.components.UiFactory;
+    import com.NeonF1.domain.DAO.PilotF1DAO;
+    import com.NeonF1.infrastructure.ui.components.Background;
+    import com.NeonF1.infrastructure.ui.components.ControlerInterface;
+    import com.NeonF1.infrastructure.ui.components.UiFactory;
 
-import javax.swing.*;
-import java.awt.*;
+    import javax.swing.*;
+    import java.awt.*;
+    import java.util.LinkedHashMap;
+    import java.util.Map;
 
-public class FormularioPanel extends Background {
-    private ControlerInterface controler;
-
-    public FormularioPanel(ControlerInterface controler) {
-        super("/Image/Pruebas.jpeg");
-        this.controler = controler;
-
-        setLayout(null);
+    public class FormularioPanel extends Background {
+        private ControlerInterface controler;
+        private Map<String, JComponent> componentesFormulario;
 
 
-        JButton botonHome = UiFactory.CrearBotom("⌂", 60, 631, 50, 50, new Color(41, 43, 58), Color.WHITE, Color.WHITE, Color.black);
-        JButton botonExit = UiFactory.CrearBotom("\uD83D\uDD0D", 110, 631, 50, 50, new Color(41, 43, 58), Color.WHITE, Color.WHITE, Color.black);
-        JButton Boton1 = UiFactory.CrearBotom("\uD83C\uDFCE\uFE0F", 160, 631, 50, 50, new Color(41, 43, 58), Color.WHITE, Color.WHITE, Color.black);
-        JButton Boton2 = UiFactory.CrearBotom("\uD83D\uDE99", 210, 631, 50, 50, new Color(41, 43, 58), Color.WHITE, Color.WHITE, Color.black);
-        JButton Boton3 = UiFactory.CrearBotom("\uD83C\uDFC1", 260, 631, 50, 50, new Color(41, 43, 58), Color.WHITE, Color.WHITE, Color.black);
+        private Map<String, JTextField> camposEquipo;
+        public FormularioPanel(ControlerInterface controler) {
+            super("/Image/Pruebas.jpeg");
+            this.controler = controler;
 
-        add(botonHome);
-        add(botonExit);
-        add(Boton1);
-        add(Boton2);
-        add(Boton3);
+            setLayout(null);
 
-        botonHome.addActionListener(e -> controler.cambiarPanel("MenuInicio") );
+            Map<String, Class<?>> campos = new LinkedHashMap<>();
+            campos.put("año", Integer.class);
+            campos.put("equipo", String.class);
+            campos.put("nombre", String.class);
+            campos.put("n_Piloto", Integer.class);
+            campos.put("pilotoPrincipal", Boolean.class);
+            campos.put("ganadorMundial", Boolean.class);
+            campos.put("posicionCampeonato", Integer.class);
+            campos.put("puntosCampeonato", Integer.class);
+            campos.put("victorias", Integer.class);
+            campos.put("poles", Integer.class);
+            campos.put("N_campeonatos", Integer.class);
+            campos.put("total_Carreras", Integer.class);
+
+            componentesFormulario = UiFactory.CrearFormularioInteligente(this, campos, 60, 40, 400, 450, piloto -> {
+
+                if ( PilotF1DAO.InsertPilot(piloto) == true) {
+                    JOptionPane.showMessageDialog(this, "Piloto " + piloto.getNombre() + " guardado");
+                }
+                else {
+                    JOptionPane.showMessageDialog(this, "Fallo el guardado del piloto " + piloto.getNombre() + " revisa la peticion");
+                };
+            });
+
+            JButton botonHome = UiFactory.CrearBotom("⌂", 60, 631, 50, 50, new Color(41, 43, 58), Color.WHITE, Color.WHITE, Color.black);
+            JButton botonExit = UiFactory.CrearBotom("\uD83D\uDD0D", 110, 631, 50, 50, new Color(41, 43, 58), Color.WHITE, Color.WHITE, Color.black);
+            JButton Boton1 = UiFactory.CrearBotom("\uD83C\uDFCE\uFE0F", 160, 631, 50, 50, new Color(41, 43, 58), Color.WHITE, Color.WHITE, Color.black);
+            JButton Boton2 = UiFactory.CrearBotom("\uD83D\uDE99", 210, 631, 50, 50, new Color(41, 43, 58), Color.WHITE, Color.WHITE, Color.black);
+            JButton Boton3 = UiFactory.CrearBotom("\uD83C\uDFC1", 260, 631, 50, 50, new Color(41, 43, 58), Color.WHITE, Color.WHITE, Color.black);
+
+            add(botonHome);
+            add(botonExit);
+            add(Boton1);
+            add(Boton2);
+            add(Boton3);
+
+            botonHome.addActionListener(e -> controler.cambiarPanel("MenuInicio") );
+        }
+
     }
-
-
-}
