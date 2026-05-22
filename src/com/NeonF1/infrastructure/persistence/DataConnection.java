68a88aed3245869ed46fa490b0d29b525e823d1d
sebas2287ti <Sebas2287ti@gmail.com>
2026-05-21 22:14:58 -0500

    Se migra las variables de coneccion a un .env

--- src/com/NeonF1/infrastructure/persistence/DataConnection.java
+++ src/com/NeonF1/infrastructure/persistence/DataConnection.java
@@ -1,13 +1,15 @@
 package com.NeonF1.infrastructure.persistence;
+
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.DriverManager;
 
 public class DataConnection {
-    private static final String KEY = "Quemirasapo";
-    private static final String USER = "neondb_owner";
-    private static final String URL = "jdbc:postgresql://ep-divine-poetry-aqdv9xsc-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channelBinding=require";
+    private static final String KEY = System.getenv("DB_PASSWORD");
+    private static final String USER = System.getenv("DB_USER");
+    private static final String URL = System.getenv("DB_URL");
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, KEY);
