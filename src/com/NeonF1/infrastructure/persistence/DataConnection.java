09608f5e91f45e456692eaa3a5e9691f5633c693
sebas2287ti <Sebas2287ti@gmail.com>
2026-05-22 07:43:14 -0500

    Comentarios y arreglo de la estructura

--- src/com/NeonF1/infrastructure/persistence/DataConnection.java
+++ src/com/NeonF1/infrastructure/persistence/DataConnection.java
@@ -1,16 +1,17 @@
 package com.NeonF1.infrastructure.persistence;
 
-
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.DriverManager;
 
 public class DataConnection {
+    //Credenciales para la conexion con la base de datos (guardadas en el .env)
     private static final String KEY = System.getenv("DB_PASSWORD");
     private static final String USER = System.getenv("DB_USER");
     private static final String URL = System.getenv("DB_URL");
 
+
+    //Metodo para realizar la conexion a la base de datos
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, KEY);
     }
